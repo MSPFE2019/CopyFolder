78c791a2f462f2e212cfe25b3cb058f76a49920a
Mose <57810061+MSPFE2019@users.noreply.github.com>
2023-02-02 23:06:57 -0600

    Update CopyFolders.ps1

--- CopyFolders.ps1
+++ CopyFolders.ps1
@@ -1,62 +1,43 @@
-#Connect to PnP Online
+# Connect to PnP Online
 $SiteURL = "https://tam3.sharepoint.com/"
 Connect-PnPOnline -Url $SiteURL -UseWebLogin
 
+# Import CSV file
 $csv = Import-Csv C:\Test.csv 
+
+# Loop through each row of the CSV file
 $csv | ForEach-Object {
-    $source = $_.source
-    $Dest = $_.Dest
-   }
+  $source = $_.source
+  $Dest = $_.Dest
+  
+  # Create URLs for the source and destination folders
+  $SourceFolderURL = "$SiteURL" + "Shared Documents/" + "$source"
+  $DestFolderURL = "$SiteURL" + "$Dest" + "/" + "$source"
 
-$i = 0
-$csv | ForEach-Object {
-$source = $_.source
-$Dest = $_.Dest 
+  # Check if the destination folder already exists
+  $DestFolder = Get-PnPFolder -Url $DestFolderURL -ErrorAction SilentlyContinue
 
-$SourceFolderURL = $siteurl + "Shared Documents/" + $source
-$folderurl = $Dest + "/" + $source
+  # If the destination folder does not exist, create it
+  if (!$DestFolder) {
+    $folder = Add-PnPFolder -Name $source -Folder $Dest -ErrorAction Stop
+    Write-host -f Green "New Folder '$dest' Added!"
+  } else {
+    Write-host "Folder $Dest Already Exist"  
+  }
 
-$Check = "/" + "Shared Documents/" + $source
+  # Copy the source folder to the destination folder
+  Write-Host "****************************************"
+  Write-Host  "Source Location : $SourceFolderURL"
+  Write-Host  "Destination Location : $DestFolderURL"
+  Write-Host "****************************************"
+  
+  Copy-PnPFile -SourceUrl $SourceFolderURL -TargetUrl $DestFolderURL -force -ErrorAction SilentlyContinue
 
-####Creating folder
-Try {
- $folder = Add-PnPFolder -Name $source -Folder $Dest -ErrorAction Stop
-    Write-host -f Green "New Folder '$dest' Added!" 
-	$TargetFolderURL ="/" + $Dest + "/" + $source
-
-Write-Host "****************************************"
-Write-Host  "Source Location : $SourceFolderURL"
-Write-Host  "Destination Location $TargetFolderURL"
-Write-Host "****************************************"
-
-Copy-PnPFile -SourceUrl $SourceFolderURL -TargetUrl $TargetFolderURL -force -ErrorAction SilentlyContinue -ErrorVariable error
-
-$D = Resolve-PnPFolder -SiteRelativePath $TargetFolderURL 
-$s = Resolve-PnPFolder -SiteRelativePath $Check 
+  # Get the number of items in the destination folder
+  $destinationItemCount = (Get-PnPFolderItem -FolderSiteRelativeUrl $DestFolderURL).Count
+  Write-Host "Destination Folder Item Count: $destinationItemCount"
 }
 
-catch {
- $2 = Resolve-PnPFolder -SiteRelativePath $FolderURL
- Write-host "Folder $Dest Already Exist"  
- $TargetFolderURL ="/" + $Dest + "/" + $source
-
-
-Write-Host "****************************************"
-Write-Host  "Source Location : $SourceFolderURL"
-Write-Host  "Destination Location $TargetFolderURL"
-Write-Host "****************************************"
-
-Copy-PnPFile -SourceUrl $SourceFolderURL -TargetUrl $TargetFolderURL -force -ErrorAction SilentlyContinue -ErrorVariable error
-
-
-$D = Resolve-PnPFolder -SiteRelativePath $TargetFolderURL 
-$s = Resolve-PnPFolder -SiteRelativePath $Check 
-}
-$count
-
-$D , $S |out-gridview
+# Show a message box to indicate that the script has completed
 $Shell = New-Object -ComObject "WScript.Shell"
-$Button = $Shell.Popup("Click OK to continue, if you Count equals.", 0, "Check your count!!", 0)
-}
-
-
+$Button = $Shell.Popup("Script Completed. Check the destination folder item count.", 0, "Check your count!!", 0)
